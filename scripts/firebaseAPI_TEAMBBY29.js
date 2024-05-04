b3873e8c501e470c480a90510a38d5ba3c9d6702
Linh Hoang <linhoang.ce@gmail.com>
2024-05-04 11:04:02 -0700

    added api key to gitignore

--- scripts/firebaseAPI_TEAMBBY29.js
+++ scripts/firebaseAPI_TEAMBBY29.js
@@ -1,1 +1,24 @@
-
+//----------------------------------------
+// This code is reused from the previous project.
+// Credits: Course 1800 - Instructor Ms Carly Orr
+//  FitUp web app's Firebase configuration
+//----------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyCYZHuxwD2nETWwgc6eH11uXmlKlx2GyHo",
+    authDomain: "comp1800-bby14-b87af.firebaseapp.com",
+    projectId: "comp1800-bby14-b87af",
+    storageBucket: "comp1800-bby14-b87af.appspot.com",
+    messagingSenderId: "146512146276",
+    appId: "1:146512146276:web:c76e20f4075f1bc8f5d471",
+    measurementId: "G-9M2Q3B13XP"
+  };
+  
+  //--------------------------------------------
+  // initialize the Firebase app
+  // initialize Firestore database if using it
+  //--------------------------------------------
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const storage = firebase.storage();
+  
+  
