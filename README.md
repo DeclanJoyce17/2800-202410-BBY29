6e0d0b762ac2e3bd740f4b40d21a8a3d135e1430
Yuho Lim <dbgh7894@gmail.com>
2024-05-06 11:12:51 -0700

    added a leaderboards, changed icons.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BBY-29
+
+main - leaderboard : https://codepen.io/MalunariDev/pen/RweGGxx
+main - icon : https://www.strangeicons.com/
+main - gold medal icon : https://github.com/malunaridev/Challenges-iCodeThis/blob/master/4-leaderboard/assets/gold-medal.png?raw=true
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
+// User successfully signed in.
+// display username. 
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName);  //print the user name in the browser console
+            username = user.displayName;
+
+            // method #1:  insert with JS
+            document.getElementById("name-goes-here").innerText = username;
+
+        } else {
+            // No user is signed in.
+            console.log("No user is logged in");
+        }
+    });
+}
+getNameFromAuth(); //run the function
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize Firebase Auth and Firestore
     const db = firebase.firestore();
