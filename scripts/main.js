3fc8dc127b957da46ffdb9e719db441f5120cc2c
Linh Hoang <linhoang.ce@gmail.com>
2024-05-28 20:49:45 -0700

    Style main page: Header, To Do lists, AI Model Configuration, bottom navbar.

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const uploadSuccess = urlParams.get('upload');
+
+    if (uploadSuccess === 'success') {
+      const successMessageDiv = document.getElementById('success-message');
+      successMessageDiv.style.display = 'block';
+
+      // Optionally, refresh the page after a short delay
+      setTimeout(() => {
+        window.location.href = window.location.pathname;
+      }, 3000); // Refresh after 3 seconds
+    }
+  });
+
+  // Hide the background image and display the dropdown for AI chat configuration
+  const initBtn = document.querySelector('.index-btn');
+  const aiChatConfig = document.querySelector('.d-inline-flex');
+  initBtn.addEventListener('click', () => {
+    initBtn.style.display = 'none';
+    document.getElementById('ai-chat-bg').style.display = 'none';
+    aiChatConfig.style.transition = '2s';
+  })
